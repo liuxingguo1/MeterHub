01c5647c469423b4d9452a75341fca96c52bf1eb
bestaone <bestaone@163.com>
2024-03-30 16:27:34 +0800

    调整

---
+++ meterhub-worker/src/main/java/com/vking/duhv/meterhub/core/Constant.java
@@ -0,0 +1,15 @@
+package com.vking.duhv.meterhub.core;
+
+public class Constant {
+
+    public static final String DATA_PROTOCOL_JSON = "JSON";
+
+    public static final String DATA_PROTOCOL_IEC000 = "IEC000";
+
+    public static final String DATA_PROTOCOL_IEC103 = "IEC103";
+
+    public static final String DATA_PROTOCOL_IEC104 = "IEC104";
+
+    public static final String TOPIC_SHENGYUDIANLIU = "meterhub-worker_shengyudianliu";
+
+}
