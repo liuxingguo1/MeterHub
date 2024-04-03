8592610b3bea50accde394cddeda2c44a282ec16
zhangsongyu <none>
2024-04-03 15:23:44 +0800

    代码优化

--- meterhub-client/src/main/java/com/vking/duhv/meterhub/client/core/TCPConnection.java
+++ meterhub-client/src/main/java/com/vking/duhv/meterhub/client/core/TCPConnection.java
@@ -12,6 +12,7 @@ import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.handler.codec.bytes.ByteArrayDecoder;
 import io.netty.handler.codec.bytes.ByteArrayEncoder;
 import io.netty.handler.timeout.IdleStateHandler;
+import jakarta.annotation.PreDestroy;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.extern.slf4j.Slf4j;
@@ -25,8 +26,7 @@ public class TCPConnection extends SubSystemConnection {
     @JsonIgnore
     private Class<? extends ByteToMessageDecoder> decoderClazz;
     @JsonIgnore
-    private ByteToMessageDecoder decoder;
-
+    private Bootstrap bootstrap;
     @JsonIgnore
     private SimpleChannelFutureListener listener = new SimpleChannelFutureListener();
 
@@ -48,8 +48,6 @@ public class TCPConnection extends SubSystemConnection {
     @JsonIgnore
     private Class<? extends TCPHandler> handlerClazz;
 
-    @JsonIgnore
-    private TCPHandler tcpHandler = null;
 
     @JsonIgnore
     private MeterHubServerClient meterHubServerClient;
@@ -77,6 +75,34 @@ public class TCPConnection extends SubSystemConnection {
             log.info("{}[{}}], 主动销毁,无需重连, IP:{}, PORT:{}", config.getName(), config.getCode(), config.getHost(), config.getPort());
             return;
         }
+        bootstrap = new Bootstrap();
+        group = new NioEventLoopGroup(1);
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                /*
+                 * 是否启用心跳保活机制。在双方TCP套接字建立连接后（即都进入ESTABLISHED状态）并且在两个小时左右上层没有任何数据传输的情况下，
+                 * 这套机制才会被激活
+                 */
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                /*
+                 * 1.在TCP/IP协议中，无论发送多少数据，总是要在数据前面加上协议头，同时，对方接收到数据，也需要发送ACK表示确认。
+                 * 为了尽可能的利用网络带宽，TCP总是希望尽可能的发送足够大的数据。这里就涉及到一个名为Nagle的算法，该算法的目的就是为了尽可能发送大块数据，
+                 * 避免网络中充斥着许多小数据块。
+                 * 2.TCP_NODELAY就是用于启用或关于Nagle算法。如果要求高实时性，有数据发送时就马上发送，就将该选项设置为true关闭Nagle算法；
+                 * 如果要减少发送次数减少网络交互，就设置为false等累积一定大小后再发送。默认为false。
+                 */
+                .option(ChannelOption.TCP_NODELAY, true)
+                /*
+                 * The timeout period of the connection.
+                 * If this time is exceeded or the connection cannot be established, the connection fails.
+                 */
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3000);
+        connect();
+    }
+
+
+    public void connect() {
+        TCPHandler tcpHandler = null;
         try {
             tcpHandler = handlerClazz.getDeclaredConstructor().newInstance();
         } catch (Exception e) {
@@ -85,6 +111,7 @@ public class TCPConnection extends SubSystemConnection {
             return;
         }
         tcpHandler.init(this);
+        ByteToMessageDecoder decoder = null;
         if (null != decoderClazz) {
             try {
                 decoder = decoderClazz.getDeclaredConstructor().newInstance();
@@ -94,61 +121,32 @@ public class TCPConnection extends SubSystemConnection {
                 return;
             }
         }
-        doStart();
+        ByteToMessageDecoder finalDecoder = decoder;
+        TCPHandler finalTcpHandler = tcpHandler;
+        bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            public void initChannel(SocketChannel ch) throws Exception {
+                ChannelPipeline pipeline = ch.pipeline();
+                if (null != finalDecoder) {
+                    pipeline.addLast(finalDecoder);
+                }
+                pipeline.addLast(new ByteArrayEncoder());
+                pipeline.addLast(new ByteArrayDecoder());
+                pipeline.addLast(new IdleStateHandler(0, 5, 0, TimeUnit.SECONDS));
+                pipeline.addLast(finalTcpHandler);
+            }
+        });
+        future = bootstrap.connect(config.getHost(), config.getPort());
+        channel = future.channel();
+        future.addListener(listener);
+
+        //对通道关闭进行监听
+        ChannelFuture closeFuture = null;
+        closeFuture = channel.closeFuture();
+        closeFuture.addListener(closeListener);
     }
 
-    private void doStart() {
-        try {
-            Bootstrap bootstrap = new Bootstrap();
-            group = new NioEventLoopGroup(1);
-            bootstrap.group(group)
-                    .channel(NioSocketChannel.class)
-                    /*
-                     * 是否启用心跳保活机制。在双方TCP套接字建立连接后（即都进入ESTABLISHED状态）并且在两个小时左右上层没有任何数据传输的情况下，
-                     * 这套机制才会被激活
-                     */
-                    .option(ChannelOption.SO_KEEPALIVE, true)
-                    /*
-                     * 1.在TCP/IP协议中，无论发送多少数据，总是要在数据前面加上协议头，同时，对方接收到数据，也需要发送ACK表示确认。
-                     * 为了尽可能的利用网络带宽，TCP总是希望尽可能的发送足够大的数据。这里就涉及到一个名为Nagle的算法，该算法的目的就是为了尽可能发送大块数据，
-                     * 避免网络中充斥着许多小数据块。
-                     * 2.TCP_NODELAY就是用于启用或关于Nagle算法。如果要求高实时性，有数据发送时就马上发送，就将该选项设置为true关闭Nagle算法；
-                     * 如果要减少发送次数减少网络交互，就设置为false等累积一定大小后再发送。默认为false。
-                     */
-                    .option(ChannelOption.TCP_NODELAY, true)
-                    /*
-                     * The timeout period of the connection.
-                     * If this time is exceeded or the connection cannot be established, the connection fails.
-                     */
-                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3000)
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            ChannelPipeline pipeline = ch.pipeline();
-                            if (null != decoder) {
-                                pipeline.addLast(decoder);
-                            }
-                            pipeline.addLast(new ByteArrayEncoder());
-                            pipeline.addLast(new ByteArrayDecoder());
-                            pipeline.addLast(new IdleStateHandler(0, 5, 0, TimeUnit.SECONDS));
-                            pipeline.addLast(tcpHandler);
-                        }
-                    });
-            future = bootstrap.connect(config.getHost(), config.getPort()).sync();
-            channel = future.channel();
-            future.addListener(listener);
-            //对通道关闭进行监听
-            ChannelFuture closeFuture = channel.closeFuture().sync();
-            closeFuture.addListener(closeListener);
-        } catch (Exception e) {
-            log.error("{}[{}}], 连接失败请求重试, IP:{}, PORT:{}", config.getName(), config.getCode(), config.getHost(), config.getPort());
-            //关闭线程组
-            group.shutdownGracefully();
-            restart();
-        }
-    }
-
-    public void restart() {
+    public void reconnect() {
         if (destroy) {
             log.info("{}[{}}], 主动销毁,无需重连, IP:{}, PORT:{}", config.getName(), config.getCode(), config.getHost(), config.getPort());
             return;
@@ -158,7 +156,7 @@ public class TCPConnection extends SubSystemConnection {
             Thread.sleep(5000L);
         } catch (InterruptedException ignore) {
         }
-        start();
+        connect();
     }
 
     public void send(String msg) {
@@ -175,6 +173,7 @@ public class TCPConnection extends SubSystemConnection {
     }
 
     @Override
+    @PreDestroy
     public void destroy() {
         destroy = true;
         if (null != group) {
@@ -211,7 +210,7 @@ public class TCPConnection extends SubSystemConnection {
 
     class SimpleChannelFutureListener implements ChannelFutureListener {
         @Override
-        public void operationComplete(ChannelFuture channelFuture) throws InterruptedException {
+        public void operationComplete(ChannelFuture channelFuture) {
             setStatus(1);
             log.info("{}[{}], 连接成功, IP:{}, PORT:{}", config.getName(), config.getCode(), config.getHost(), config.getPort());
         }
@@ -222,7 +221,7 @@ public class TCPConnection extends SubSystemConnection {
         public void operationComplete(ChannelFuture channelFuture) {
             setStatus(0);
             log.info("{}[{}], 连接中断重新连接, IP:{}, PORT:{}", config.getName(), config.getCode(), config.getHost(), config.getPort());
-            restart();
+            reconnect();
         }
     }
 
